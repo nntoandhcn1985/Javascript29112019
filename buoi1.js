5dfdb812c1f4b61d9a0d105b608d65a12b339696
nntoandhcn1985 <nntoandhcn@gmail.com>
2019-11-29 19:27:15 +0700

    update buoi1

--- buoi1.js
+++ buoi1.js
@@ -1,4 +1,7 @@
 helloReactNative
 //local cua may
 // tao ra thu muc .git => go lenh git init
-// luu thay doi : git status o local maycl
+// xem thay doi : git status o local may local
+// luu thay doi: git add buoi1.js chi luu tam (cache) trong file .git 
+// neu luu tam tat ca thi dung git add .
+// dua tap tin luu tru vao trong git: git commit -m "ghi chu ten commit vao cai gi"
